9cfcd85d4d4cc3a1d85bf121a34c18c427015532
Rodrigo <rodrigomelomarcolino@gmail.com>
2020-04-26 11:48:06 -0300

    Added react-router-dom and inital structure of application's pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return <h1></h1>;
-}
+import Routes from './routes';
+import GlobalStyles from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyles />
+  </>
+);
 
 export default App;
---
+++ src/pages/Import/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Import: React.FC = () => <h1>Import</h1>;
+
+export default Import;
